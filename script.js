5932407cef373b46c5b8a8f82dc1356d1993a625
Ejer <ejer@Ejers-MacBook-Pro.local>
2017-02-23 14:21:03 +0100

    add data navn, pris og rabatpris

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ function sidenVises() {
     // læs produktliste - linket er produktlisten fra nettte, det kan også være en direkte JSON fil man selv har oprettet
     $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
 
-
-    visProdukt();
 }
 
 // fortæller at vi vil have en liste af data for hvert produkt - den kører listen igennem et produkt af gangen
@@ -20,7 +18,15 @@ function visProdukt(produkt) {
     console.log(produkt);
     // klon produkt_template
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
+
     // indsæt data i klon
+    klon.querySelector(".data_navn").innerHTML = produkt.navn;
+    klon.querySelector(".data_pris").innerHTML = produkt.pris;
+
+    //rabatprisen udregnes - Math.ceil sætter du rundt om hele beregningen for at runde tallene op til hele tal
+    var rabatpris = Math.ceil(produkt.pris - (produkt.pris * produkt.rabatsats / 100));
+    klon.querySelector(".data_rabatpris").innerHTML = rabatpris;
+
 
     // append klon til .produkt-liste
     document.querySelector(".produktliste").appendChild(klon);
